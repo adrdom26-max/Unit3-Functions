42146e766c016b2d6194c13a6e73e73cb18af6b1
adrdom26-max <adrdom26-max>
2025-10-30 11:54:20 -0400

    lesson 1 started

---
+++ Lesson1-Introduction/basics.py
@@ -0,0 +1,57 @@
+# # Defining a function 
+# def function_name():
+#     pass
+# # Calling a function
+# function_name()
+
+# def print_header():
+#     print("="*20)
+#     print("Student Report")
+#     print("="*20)
+
+# print_header()
+
+# def draw_square():
+#     # print("*****")
+#     # print("*****")
+#     # print("*****")
+#     # print("*****")
+#     # print("*****")
+#     for i in range(5):
+#         print("*****")
+
+# draw_square()
+
+def morning_routine():
+    print("Wake up")
+    print("Get dressed")
+    print("Eat breakfast")
+
+morning_routine()
+print()
+
+def afternoon_routine():
+    print("Go to class")
+    print("Eat lunch")
+    print("Drive home")
+
+afternoon_routine()
+print()
+
+def night_routine():
+    print("Do homework")
+    print("Eat dinner")
+    print("Play games")
+
+night_routine()
+print()
+
+def daily_routine():
+    morning_routine()
+    print()
+    afternoon_routine()
+    print()
+    night_routine()
+    print()
+
+daily_routine()
