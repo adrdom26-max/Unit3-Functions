bdab193e31966f7efc480abb0c310e0df5a7cf94
adrdom26-max <adrdom26-max>
2025-10-31 10:25:31 -0400

    basics classwork

--- Lesson1-Introduction/basics.py
+++ Lesson1-Introduction/basics.py
@@ -22,36 +22,42 @@
 
 # draw_square()
 
-def morning_routine():
-    print("Wake up")
-    print("Get dressed")
-    print("Eat breakfast")
+# def morning_routine():
+#     print("Wake up")
+#     print("Get dressed")
+#     print("Eat breakfast")
 
-morning_routine()
-print()
+# morning_routine()
+# print()
 
-def afternoon_routine():
-    print("Go to class")
-    print("Eat lunch")
-    print("Drive home")
+# def afternoon_routine():
+#     print("Go to class")
+#     print("Eat lunch")
+#     print("Drive home")
 
-afternoon_routine()
-print()
+# afternoon_routine()
+# print()
 
-def night_routine():
-    print("Do homework")
-    print("Eat dinner")
-    print("Play games")
+# def night_routine():
+#     print("Do homework")
+#     print("Eat dinner")
+#     print("Play games")
 
-night_routine()
-print()
+# night_routine()
+# print()
 
-def daily_routine():
-    morning_routine()
-    print()
-    afternoon_routine()
-    print()
-    night_routine()
-    print()
+# def daily_routine():
+#     morning_routine()
+#     print()
+#     afternoon_routine()
+#     print()
+#     night_routine()
+#     print()
 
-daily_routine()
+# daily_routine()
+
+def calc_area_rect(w,l):
+    area = w * l
+    # print(area)
+    print(f"Area of {w}x{l}={area}")
+calc_area_rect(5,10)
