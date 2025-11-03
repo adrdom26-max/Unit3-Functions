9af561fac7fd8b8d163b88d88be01e44bb76688f
adrdom26-max <adrdom26-max>
2025-11-03 11:44:59 -0500

    parameters complete

---
+++ Lesson2-Parameters/parameters.py
@@ -0,0 +1,26 @@
+# def introduce(name, age, grade):
+#     print(f"This is {name}.")
+#     print(f"{name} is {age} years old.")
+#     print(f"They are in grade {grade}.")
+
+# introduce("Adrian", 17, 12)
+
+# wrong introduce(17, "Adrian", 12)
+
+# def add_numbers(num1, num2):
+#     result = num1 + num2
+#     print(f"{num1} + {num2} = {result}")
+    
+# add_numbers(20,25)
+
+# def subtract_number(num1, num2):
+#     result = num1 - num2
+#     print(f"{num1} - {num2} = {result}")
+
+# subtract_number(20,5)
+
+def calculate_percentage(score, max_score):
+    percentage = (score / max_score) * 100
+    print(f"{score}/{max_score} = {percentage}")
+
+calculate_percentage(33,100)
