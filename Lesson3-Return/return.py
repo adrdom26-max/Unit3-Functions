2e8de5270023760ed8b88dad57127b6a559b0463
adrdom26-max <adrdom26-max>
2025-11-04 11:55:49 -0500

    return started

---
+++ Lesson3-Return/return.py
@@ -0,0 +1,88 @@
+"""
+Lesson 3: Return Values - Practice Exercises
+"""
+
+# def get_getter_grade(percentage):
+#     if percentage >= 90:
+#         return "A"
+#     elif percentage >= 80:
+#         return "B"
+# result = get_getter_grade(80)
+# print() 
+
+
+# def check_age_age(age):
+#     if age >= 18:
+#         return "Adult"
+#         print("This never runs")
+#     return "Minor"
+
+# def square(num):
+#     return num ** 2 
+# result = square(5)
+# print(f"5 squared is {result}")
+# area = square(7) * 2 
+# print(f"Area: {area}")
+
+
+# def greet(name):
+#     # print(f"Hello {name}")
+#     return f"Hello {name}"
+
+# result = greet("Adrian")
+# print(result)
+
+# ============================================================================
+# Practice 1: Rectangle Area
+# ============================================================================
+# def rec_area(w,l):
+#     return l * w 
+
+# rec1 = rec_area(6,5)
+# print(rec1)
+
+
+# ============================================================================
+# Practice 2: Temperature Converter
+# ============================================================================
+# def celsius_to_fahrenheit(celsius): # (C*9/5)+32
+#     return (celsius*9/5)+32
+# temp = celsius_to_fahrenheit(20)
+# print(temp)
+
+
+# ============================================================================
+# Practice 3: Find Maximum
+# ============================================================================
+def find_max(num1,num2):
+    if num1 > num2:
+        return num1
+    return num2
+
+# ============================================================================
+# Practice 4: Is Even?
+# ============================================================================
+
+
+
+
+
+# ============================================================================
+# Practice 5: Calculate Tip
+# ============================================================================
+
+
+
+
+# ============================================================================
+# Challenge 1: Calculate Average
+# ============================================================================
+
+
+
+
+
+# ============================================================================
+# Challenge 2: Letter Grade
+# ============================================================================
+
