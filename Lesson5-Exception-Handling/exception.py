4157d73751a3fbc52e59e971ebb5a04c6c428e5d
adrdom26-max <adrdom26-max>
2025-11-17 11:52:57 -0500

    exception start

---
+++ Lesson5-Exception-Handling/exception.py
@@ -0,0 +1,79 @@
+"""
+Lesson 5: Exception Handling - Practice Exercises
+"""
+# Common exception types 
+# 1 zero division error 
+# print(10/0) 
+# 2 Type error
+# print(5+"5")
+# 3 Value error
+# print(int("bt"))
+# 4 Name error
+# print(school)
+
+# try:
+#     result = 10/0
+# except:
+#     print("Cant divide by 0")
+
+# try:
+#     num = int(input("Enter a number: "))
+#     result = 100 / num
+# except ZeroDivisionError:
+#     print("Cant divide by 0")
+# except ValueError:
+#     print("Thats not a number")
+
+
+# try:
+#     num = int(input("Enter a number: "))
+#     result = 100 / num
+# except ZeroDivisionError:
+#     print("Cant divide by 0")
+# except ValueError:
+#     print("Thats not a number")
+# else: 
+#     print(f"Result: {result}")
+# finally:
+#     print("Operation Complete")
+
+
+# ==========================
+# Practice 1: Safe Division
+# Instructions: Create a function that divides two numbers safely
+# ==========================
+# def safe_divide(a, b):
+#     try:
+#         result = a / b
+#         return result
+#     except:
+#         print("An error occured")
+#         return 0
+
+# print(safe_divide(10, 2))
+# print(safe_divide(10, 0))
+# ==========================
+# Practice 2: Safe Input
+# Instructions: Create a function that safely gets an integer from user
+# ==========================
+def get_age():
+    try:
+        age = int(input("Enter your age:" ))
+        return age
+    except ValueError:
+        print("Please enter a number")
+        return 0
+user_age = get_age()
+print(f"Your are {user_age} years old")
+
+# ==========================
+# Challenge 1: Safe Calculator
+# Instructions: Build a calculator that handles invalid input and division by zero
+# ==========================
+
+
+
+# ==========================
+# Challenge 2: Grade Input Validator
+# Instructions: Keep asking for a grade until a valid number between 0-100 is entered
+# ==========================
